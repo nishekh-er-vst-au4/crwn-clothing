95269ca4433203ad7309aeeb074b98a5a4e58618
nishekh <nishekhgowda143@gmail.com>
2020-02-07 23:10:06 +0530

    updated our menu-item navigate with Hoc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import {Route,Switch} from 'react-router-dom';
 import  Homepage from './pages/homepage/homepage.component';
 
 import './App.css';
 
+const HatsPage =()=>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route  path='/shop/hats' component={HatsPage} />
+      </Switch>
+      
     </div>
   );
 }
